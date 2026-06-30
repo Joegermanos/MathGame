93d05fce41254147830f3b037213cbb13fc96d2b
Joegermanos <joe.jermanous1@gmail.com>
2026-06-30 14:21:59 +0300

    Add program entry point

---
+++ Program.cs
@@ -0,0 +1,12 @@
+namespace MathGame
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new LoginForm());
+        }
+    }
+}
